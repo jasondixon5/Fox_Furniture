5d5e34bc68a3aae9c8d06120047b1571cd19e100
Jason Dixon <jasondixon@Jasons-MacBook-Pro.local>
2018-12-03 16:00:22 -0800

    added seed_products func to manager.command to allow populating of products table from source data

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 from app import create_app, db
 #from app.models import User, Role
-from app.models import ProductCategory
+from app.models import ProductCategory, Product
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -13,7 +13,12 @@ migrate = Migrate(app, db)
 def make_shell_context():
     #return dict(app=app, db=db, User=User, Role=Role)
     #return dict(app=app, db=db)
-    return dict(app=app, db=db, ProductCategory=ProductCategory)
+    return dict(
+    app=app, 
+    db=db, 
+    ProductCategory=ProductCategory,
+    Product=Product)
+    
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
